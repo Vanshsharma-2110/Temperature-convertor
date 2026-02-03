a50c15b592f34935517b6a3f0d778deacdd65e3f
Vanshsharma-2110 <vanshsharma211005@gmail.com>
2026-02-03 15:21:49 +0530

    Add files via upload

---
+++ prac2.js
@@ -0,0 +1,33 @@
+const textbox = document.querySelector('#textbox');
+const c_to_f = document.querySelector('#to_f');
+const f_to_c = document.querySelector('#to_c');
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+let temp;
+let ans;
+
+btn.onclick = function () {
+    temp = textbox.value;
+    temp = Number(temp);
+    console.log(temp);
+
+    if (c_to_f.checked) {
+        ans=(((9/5)*temp)+32);
+        result.textContent = truncDecimals(ans,1) +"°F";
+    }
+    
+    else if (f_to_c.checked) {
+        ans=((5/9)*(temp-32)); 
+        result.textContent = truncDecimals(ans,1) +"°C";
+    }
+
+    else {
+        result.textContent = "Select a unit";
+    }
+}
+
+function truncDecimals(num, places) {
+  const factor = 10 ** places;
+  return Math.trunc(num * factor) / factor;
+}
